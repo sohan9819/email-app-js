b9c9e7c3bf0e81d3ea77741c1dfd52d62d711554
SnickerDev <64985447+sohan9819@users.noreply.github.com>
2023-10-23 00:07:21 +0530

    Added persistent state management using localstorage

--- src/slice/user.js
+++ src/slice/user.js
@@ -1,8 +1,31 @@
 import { createSlice } from '@reduxjs/toolkit';
 
+// Function to load state from local storage
+const loadState = () => {
+  try {
+    const serializedState = localStorage.getItem('userState');
+    if (serializedState === null) {
+      return undefined;
+    }
+    return JSON.parse(serializedState);
+  } catch (error) {
+    return undefined;
+  }
+};
+
+// Function to save state to local storage
+const saveState = (state) => {
+  try {
+    const serializedState = JSON.stringify(state);
+    localStorage.setItem('userState', serializedState);
+  } catch (error) {
+    console.log('Error ', error);
+  }
+};
+
 const userSlice = createSlice({
   name: 'user',
-  initialState: {
+  initialState: loadState() || {
     readed: [],
     favourites: [],
   },
@@ -38,6 +61,12 @@ const userSlice = createSlice({
   },
 });
 
+const persistedUserReducer = (state, action) => {
+  const newState = userSlice.reducer(state, action);
+  saveState(newState);
+  return newState;
+};
+
 export const { setRead, setFavourite } = userSlice.actions;
 
-export default userSlice.reducer;
+export default persistedUserReducer;
